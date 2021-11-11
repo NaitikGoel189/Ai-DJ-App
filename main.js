a049fce8474bd1d4a874049c344ac620ed5c25e7
NaitikGoel189 <74708219+NaitikGoel189@users.noreply.github.com>
2021-11-11 16:20:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 song="";
 
+scoreLeftWrist=0;
+scoreRightWrist=0;
+
 leftWristX=0;
 leftWristY=0;
 rightWristX=0;
@@ -18,6 +21,17 @@ function setup(){
 
 function draw(){
     image(video,0,0,600,500);
+    fill("#ff0000");
+    stroke("#ff0000");
+
+    if(scoreLeftWrist>0.2){
+        circle(leftWristX, leftWristY, 20);
+        InNumberleftWristY=Number(leftWristY);
+        remove_decimals=floor(InNumberleftWristY);
+        volume=remove_decimals/500;
+        document.getElementById("volume").innerHTML="volume= "+volume;
+        song.setVolume(volume);
+    }
 }
 
 function preload(){
@@ -38,6 +52,8 @@ function gotPoses(results){
 if(results.length>0){
 console.log(results);
 
+scoreLeftWrist = results[0].pose.keypoints[9].score
+console.log("scoreLeftWrist= "+scoreLeftWrist);
 leftWristX=results[0].pose.leftWrist.x;
 leftWristY=results[0].pose.leftWrist.y;
 console.log("leftWristX= " + leftWristX + ", leftWristY= " + leftWristY);
